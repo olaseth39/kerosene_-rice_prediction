fd0c8b7ec88ba074dbed7eed3917ea68335ff075
Abiola <olaseth39@gmail.com>
2020-09-04 00:53:44 +0100

    added some files

--- server.py
+++ server.py
@@ -1,10 +1,13 @@
-from flask import Flask, request, jsonify
+from flask import Flask, request, jsonify, render_template
 import util
 
 app = Flask(__name__)
 
-# to get all the the month
+@app.route("/")
+def home():
+    return render_template('app.html')
 
+# to get all the the month
 
 @app.route("/month_names")
 def get_month_names():
